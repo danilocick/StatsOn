92d6b9974f4f77e1860b1e7c37ed96b807bb21ac
danilocick <56125505+danilocick@users.noreply.github.com>
2020-12-12 21:20:49 +0100

    RivalFragment.java

--- app/src/main/java/com/example/mp07_statson/MenuResultFragment.java
+++ app/src/main/java/com/example/mp07_statson/MenuResultFragment.java
@@ -45,7 +45,7 @@ public class MenuResultFragment extends Fragment {
         finishedLoading.observe(getViewLifecycleOwner(), new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean aBoolean) {
-                navController.navigate(R.id.action_menuResultFragment_to_resultadoMenuFragment);
+                navController.navigate(R.id.action_menuResultFragment_to_listaJugadoresFragment);
             }
         });
 
