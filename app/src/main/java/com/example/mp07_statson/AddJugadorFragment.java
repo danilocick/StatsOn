989f5e8cba41f5cabcfaedcbdb9ffcdf28f0542a
danilocick <56125505+danilocick@users.noreply.github.com>
2020-12-14 21:22:51 +0100

    Navigations

--- app/src/main/java/com/example/mp07_statson/AddJugadorFragment.java
+++ app/src/main/java/com/example/mp07_statson/AddJugadorFragment.java
@@ -2,63 +2,58 @@ package com.example.mp07_statson;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddJugadorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.mp07_statson.databinding.FragmentAddJugadorBinding;
+import com.example.mp07_statson.databinding.FragmentEquipoBBinding;
+
 public class AddJugadorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button botonComebackAddJ;
+    Button botonCrearAddJ;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private NavController navController;
+    private FragmentAddJugadorBinding binding;
 
-    public AddJugadorFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddJugadorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddJugadorFragment newInstance(String param1, String param2) {
-        AddJugadorFragment fragment = new AddJugadorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_jugador, container, false);
+        return (binding = FragmentAddJugadorBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        botonComebackAddJ = view.findViewById(R.id.botonComeBackAddJ);
+        botonCrearAddJ = view.findViewById(R.id.botonCrearAddJ);
+
+        //ComeBack
+        botonComebackAddJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_addJugadorFragment_to_equipoBFragment);
+            }
+        });
+
+        //Crear Jugador
+        botonCrearAddJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_addJugadorFragment_to_equipoBFragment);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mp07_statson/OptionsFragment.java
+++ app/src/main/java/com/example/mp07_statson/OptionsFragment.java
@@ -2,63 +2,57 @@ package com.example.mp07_statson;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OptionsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.mp07_statson.databinding.FragmentAddJugadorBinding;
+import com.example.mp07_statson.databinding.FragmentOptionsBinding;
+
 public class OptionsFragment extends Fragment {
+    Button botonComebackOpc;
+    Button botonGuardarOpc;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private NavController navController;
+    private FragmentOptionsBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OptionsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OptionsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OptionsFragment newInstance(String param1, String param2) {
-        OptionsFragment fragment = new OptionsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_options, container, false);
+        return (binding = FragmentOptionsBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        botonComebackOpc = view.findViewById(R.id.botonComeBackOpc);
+        botonGuardarOpc = view.findViewById(R.id.botonGuardarOpc);
+
+        //ComeBack
+        botonComebackOpc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_optionsFragment_to_menuFragment);
+            }
+        });
+
+        //Crear Jugador
+        botonGuardarOpc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_optionsFragment_to_menuFragment);
+            }
+        });
+
     }
 }
