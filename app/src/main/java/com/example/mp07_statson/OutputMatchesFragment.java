5babcf32d901f791d245caa7f3ebe2a8719c8e39
danilocick <dhernandez@elpuig.xeill.net>
2021-05-20 17:43:26 +0200

    proceso tabla

--- app/src/main/java/com/example/mp07_statson/OutputMatchesFragment.java
+++ app/src/main/java/com/example/mp07_statson/OutputMatchesFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.mp07_statson.Model.Jugador;
 import com.example.mp07_statson.databinding.FragmentOutputMatchesBinding;
+
+import org.xmlpull.v1.XmlPullParser;
 
 public class OutputMatchesFragment extends BaseFragment {
 
@@ -23,8 +28,33 @@ public class OutputMatchesFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        for (Jugador j : partidoviewmodel.jugadoresEquipoLocal) {
 
+            String[] header = {"Dorsal", "Nombre", "Min", "PTS", "TL", "","","T2", "","","T3","","", "Rebotes","","", "Faltas","", "Balones","", "Tapones","",
+                    "Pasos", "Dobles", "V.T", "As", "Val"};
 
+            TableRow row=new TableRow(requireActivity());
+            String dorsal = String.valueOf(j.dorsal);
+            String nombre = String.valueOf(j.nombre);
+            String puntos = String.valueOf(j.puntos);
+            TextView tv1=new TextView(requireActivity());
+            tv1.setText(dorsal);
+            TextView tv2=new TextView(requireActivity());
+            tv2.setText(puntos);
+
+            TextView tv3=new TextView(requireActivity());
+            tv3.setText(nombre);
+//            TextView tv1=new TextView(requireActivity());
+//            tv1.setText(dorsal);
+//            TextView tv1=new TextView(requireActivity());
+//            tv1.setText(dorsal);
+//            TextView tv1=new TextView(requireActivity());
+//            tv1.setText(dorsal);
+//            row.addView(tv1);
+//            row.addView(tv2);
+
+            binding.table.addView(row);
+        }
 
         binding.botonComeBack.setOnClickListener(view1 -> nav.popBackStack());
 
